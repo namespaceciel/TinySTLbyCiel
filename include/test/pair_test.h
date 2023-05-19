ee8b9bed7ebbacb6643f1766d68ef95468f8caa0
Ciel <manipulateusall@gmail.com>
2023-05-20 00:35:11 +0800

    2023.5.20

--- include/test/pair_test.h
+++ include/test/pair_test.h
@@ -9,19 +9,39 @@
 #endif
 
 namespace pair_test_details {
-	struct empty {
-		const empty& operator=(const empty&) const { return *this; }
-		const empty& operator=(empty&&) const { return *this; }
+	struct empty_struct {
+		const empty_struct& operator=(const empty_struct&) const { return *this; }
+		const empty_struct& operator=(empty_struct&&) const { return *this; }
+	};
+
+	struct mutable_struct {
+		mutable int a{};
+		const mutable_struct& operator=(const mutable_struct& other) const {
+			a = other.a;
+			return *this;
+		}
+		const mutable_struct& operator=(mutable_struct&& other) const {
+			a = ciel::move(other.a);
+			return *this;
+		}
 	};
 }
 
 void pair_test() {
 	using namespace pair_test_details;
 
-	const ciel::pair<empty, empty> p1;
-	const ciel::pair<empty, empty> p2;
-	p1 = p2;
-	p1 = ciel::move(p2);
+	{
+		const ciel::pair<empty_struct, empty_struct> p1;
+		const ciel::pair<empty_struct, empty_struct> p2;
+		p1 = p2;
+		p1 = ciel::move(p2);
+	}
+	{
+		const ciel::pair<mutable_struct, mutable_struct> p3;
+		const ciel::pair<mutable_struct, mutable_struct> p4;
+		p3 = p4;
+		p3 = ciel::move(p4);
+	}
 
 	std::cout << "All pair_tests finished.\n\n";
 }
